b3f5e637645559e7d66b4d047833f1b6c17e162a
Guy Avraham <avraham.guy2@gmail.com>
2025-09-04 15:16:06 +0300

    Fix test configuration and add graddle wrapper

--- backend/src/test/java/com/guyavraham/cryptoadvisor/cryptoadvisorbackend/BackendApplicationTests.java
+++ backend/src/test/java/com/guyavraham/cryptoadvisor/cryptoadvisorbackend/BackendApplicationTests.java
@@ -2,7 +2,9 @@ package com.guyavraham.cryptoadvisor.cryptoadvisorbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
